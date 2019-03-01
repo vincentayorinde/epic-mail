e0e049e2d716e945d3b5cf6fd86d9377abe3f3a2
vincentayorinde <vincentayorinde@yahoo.com>
2019-03-01 21:03:45 +0100

    add mailbox.html pages

--- ui/assets/js/main.js
+++ ui/assets/js/main.js
@@ -1,19 +1,3 @@
-
-// const validate = () => {
-// const email = document.getElementById('email').value;
-// const regxEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|// (([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-//   // if (regxEmail.test(email)) {
-//   //   document.getElementById('labeltext').innerHTML = 'valid';
-//   //   document.getElementById('labeltext').style.visibility = 'visible';
-//   //   document.getElementById('labeltext').style.color = 'green';
-//   // } else {
-//   //   document.getElementById('labeltext').innerHTML = 'invalid';
-//   //   document.getElementById('labeltext').style.visibility = 'visible';
-//   //   document.getElementById('labeltext').style.color = 'red';
-//   // }
-// };
-// validate();
 const showProfileMenu = () => {
   const profileMenu = document.getElementById('profile-menu');
   if (profileMenu.style.display === 'none') {
@@ -23,3 +7,8 @@ const showProfileMenu = () => {
   }
 };
 showProfileMenu();
+
+const loadContent = (page) => {
+  document.getElementById('content').innerHTML = `<object type='text/html' data='mailbox/${page}.html' ></object>`;
+};
+loadContent('inbox');
