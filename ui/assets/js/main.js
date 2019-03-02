8687f023297cd6507492f4d9a86f304e9c36f38b
vincentayorinde <vincentayorinde@yahoo.com>
2019-03-02 17:51:05 +0100

    Update mailbox.html and main.css

--- ui/assets/js/main.js
+++ ui/assets/js/main.js
@@ -10,5 +10,16 @@ showProfileMenu();
 
 const loadContent = (page) => {
   document.getElementById('content').innerHTML = `<object type='text/html' data='mailbox/${page}.html' ></object>`;
+  document.getElementById('mySidenav').style.width = '0';
 };
 loadContent('inbox');
+
+const openNav = () => {
+  document.getElementById('mySidenav').style.width = '250px';
+};
+openNav();
+
+const closeNav = () => {
+  document.getElementById('mySidenav').style.width = '0';
+};
+closeNav();
