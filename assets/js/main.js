4960b72d8c8dfbfa8334620fd43bac6cc6f56bc6
vincentayorinde <vincentayorinde@yahoo.com>
2019-03-02 17:16:30 +0100

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,13 @@ const loadContent = (page) => {
   document.getElementById('content').innerHTML = `<object type='text/html' data='mailbox/${page}.html' ></object>`;
 };
 loadContent('inbox');
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById('mySidenav').style.width = '250px';
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById('mySidenav').style.width = '0';
+}
