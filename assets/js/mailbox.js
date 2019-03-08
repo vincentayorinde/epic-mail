3ad18f0787364809a59ef1ec54c3c36d00de12b4
vincentayorinde <vincentayorinde@yahoo.com>
2019-03-08 15:13:15 +0100

    Updates

---
+++ assets/js/mailbox.js
@@ -0,0 +1,11 @@
+
+
+const openNav = () => {
+  document.getElementById('mySidenav').style.width = '250px';
+};
+openNav();
+
+const closeNav = () => {
+  document.getElementById('mySidenav').style.width = '0';
+};
+closeNav();
