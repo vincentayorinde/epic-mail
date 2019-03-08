b3eb1df32982ad534063a31dde4b42eeca9a95dd
vincentayorinde <vincentayorinde@yahoo.com>
2019-03-08 23:58:21 +0100

    Updates

--- assets/js/mailbox.js
+++ assets/js/mailbox.js
@@ -8,3 +8,11 @@ const closeNav = () => {
   document.getElementById('mySidenav').style.width = '0';
 };
 closeNav();
+
+const showSearchMobile = () => {
+  document.getElementById('searchMobile').style.visibility = 'visible';
+  document.getElementById('searchMobile').style.transition = '1s';
+};
+const closeSearchMobile = () => {
+  document.getElementById('searchMobile').style.visibility = 'hidden';
+};
