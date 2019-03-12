4dea263667f95e98da0fd36a594899bcb6ec58bd
vincentayorinde <vincentayorinde@yahoo.com>
2019-03-12 15:50:26 +0100

    ft(endpoint): create or send a mail feature
    - create method for send mails
    - add test for this endpoint /api/v1/messages
    - add input validations for this endpoint
    
    [Delivers #164561857]

--- test/messages.test.js
+++ test/messages.test.js
@@ -7,6 +7,33 @@ chai.use(chaiHttp);
 
 const should = chai.should();
 
+let newmessage;
+
+
+describe('POST /messages', () => {
+  it('Should create or send an email on /api/v1/messages POST', (done) => {
+    const message = {
+      createdOn: new Date(),
+      subject: 'Sample message title',
+      message: 'Sample message content',
+      senderId: '3',
+      receiverId: '5',
+      parentMessageId: [],
+      status: 'unread',
+      isDeleted: false,
+    };
+    chai.request(app)
+      .post('/api/v1/messages')
+      .send(message)
+      .end((err, res) => {
+        newmessage = res.body.message;
+        expect(res).to.have.status(201);
+        expect(res.body.message).to.equal('Message sent successfully');
+        expect(res.body).to.be.a('object');
+        done();
+      });
+  });
+});
 
 describe('GET /messages', () => {
   it('Should get all receive mails on /api/v1/messages GET', (done) => {
