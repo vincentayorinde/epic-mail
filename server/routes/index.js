148148c82ebe390dc93f53b6eb4ec771744b162d
vincentayorinde <vincentayorinde@yahoo.com>
2019-03-22 00:43:43 +0100

    chore(ch): Convert v1 endpoints to v2
    - update routes to have /v2
    - create v2 server folder
    - move old endpoints to /v1 folder
    
    [Finishes #164818822]

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,33 @@
 import express from 'express';
-import User from '../controllers/users';
-import Message from '../controllers/messages';
+import User from '../v2/controllers/user';
+import Message from '../v2/controllers/message';
+import Auth from '../v2/middleware/auth';
 
 const router = express.Router();
 
-router.post('/api/v1/auth/signup', User.createUser);
-router.post('/api/v1/auth/signin', User.userLogin);
-router.post('/api/v1/messages', Message.sendMail);
-router.get('/api/v1/messages', Message.getAllMails);
-router.get('/api/v1/messages/unread/:receiverId', Message.getAllUnreadMailByUser);
-router.get('/api/v1/messages/sent/:senderId', Message.getAllMailsSentByUser);
-router.get('/api/v1/messages/:messageId', Message.getAMailRecord);
-router.delete('/api/v1/messages/:messageId', Message.deleteUserMail);
+// Create or add user
+router.post('/api/v2/auth/signup', User.signUp);
+
+// Login user
+router.post('/api/v2/auth/login', User.loginUser);
+
+// Send or create a message
+router.post('/api/v2/messages', Message.sendMail);
+
+// Get all messages
+router.get('/api/v2/messages', Message.getAllMails);
+
+// Get all user unread messages
+router.get('/api/v2/messages/unread', Message.getAllUnreadMailByUser);
+
+// Get all user sent messages
+router.get('/api/v2/messages/sent', Message.getAllMailsSentByUser);
+
+// Get a specific mail record
+router.get('/api/v2/messages/:messageId', Message.getAMailRecord);
+
+// // Delete a specific mail record
+router.delete('/api/v2/messages/:messageId', Message.deleteUserMail);
 
 
 module.exports = router;
