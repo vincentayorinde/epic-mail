0d298de74ea5f13993eaddedd33a8a2e14d833c9
vincentayorinde <vincentayorinde@yahoo.com>
2019-03-22 01:07:24 +0100

    feat(Auth): Implement JWT for Auth
    - update app.js file to inclue environment variable
    - add helper.js file
    - update the user controller
    
    [Finishes #164729825]

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import User from '../v2/controllers/user';
-import Message from '../v2/controllers/message';
-import Auth from '../v2/middleware/auth';
 
 const router = express.Router();
 
@@ -11,23 +9,23 @@ router.post('/api/v2/auth/signup', User.signUp);
 // Login user
 router.post('/api/v2/auth/login', User.loginUser);
 
-// Send or create a message
-router.post('/api/v2/messages', Message.sendMail);
+// // Send or create a message
+// router.post('/api/v2/messages', Message.sendMail);
 
-// Get all messages
-router.get('/api/v2/messages', Message.getAllMails);
+// // Get all messages
+// router.get('/api/v2/messages', Message.getAllMails);
 
-// Get all user unread messages
-router.get('/api/v2/messages/unread', Message.getAllUnreadMailByUser);
+// // Get all user unread messages
+// router.get('/api/v2/messages/unread', Message.getAllUnreadMailByUser);
 
-// Get all user sent messages
-router.get('/api/v2/messages/sent', Message.getAllMailsSentByUser);
+// // Get all user sent messages
+// router.get('/api/v2/messages/sent', Message.getAllMailsSentByUser);
 
-// Get a specific mail record
-router.get('/api/v2/messages/:messageId', Message.getAMailRecord);
+// // Get a specific mail record
+// router.get('/api/v2/messages/:messageId', Message.getAMailRecord);
 
-// // Delete a specific mail record
-router.delete('/api/v2/messages/:messageId', Message.deleteUserMail);
+// // // Delete a specific mail record
+// router.delete('/api/v2/messages/:messageId', Message.deleteUserMail);
 
 
 module.exports = router;
