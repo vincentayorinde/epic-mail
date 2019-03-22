78c99aa95e516b631eee3c8a6e9e934b2b8511dd
vincentayorinde <vincentayorinde@yahoo.com>
2019-03-22 01:34:37 +0100

    feat(endpoint): Create Group
    - add group.js controller
    - update message controller
    - add group table
    
    [Finishes #164729287]

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import User from '../v2/controllers/user';
+import Message from '../v2/controllers/message';
+import Group from '../v2/controllers/group';
+import Auth from '../v2/middleware/auth';
 
 const router = express.Router();
 
@@ -9,23 +12,27 @@ router.post('/api/v2/auth/signup', User.signUp);
 // Login user
 router.post('/api/v2/auth/login', User.loginUser);
 
-// // Send or create a message
-// router.post('/api/v2/messages', Message.sendMail);
+// Send or create a message
+router.post('/api/v2/messages', Auth.verifyToken, Message.sendMail);
 
-// // Get all messages
-// router.get('/api/v2/messages', Message.getAllMails);
+// Get all messages
+router.get('/api/v2/messages', Auth.verifyToken, Message.getAllMails);
 
-// // Get all user unread messages
-// router.get('/api/v2/messages/unread', Message.getAllUnreadMailByUser);
+// Get all user unread messages
+router.get('/api/v2/messages/unread', Auth.verifyToken, Message.getAllUnreadMailByUser);
 
-// // Get all user sent messages
-// router.get('/api/v2/messages/sent', Message.getAllMailsSentByUser);
+// Get all user sent messages
+router.get('/api/v2/messages/sent', Auth.verifyToken, Message.getAllMailsSentByUser);
 
-// // Get a specific mail record
-// router.get('/api/v2/messages/:messageId', Message.getAMailRecord);
+// Get a specific mail record
+router.get('/api/v2/messages/:messageId', Auth.verifyToken, Message.getAMailRecord);
 
-// // // Delete a specific mail record
-// router.delete('/api/v2/messages/:messageId', Message.deleteUserMail);
+// // Delete a specific mail record
+router.delete('/api/v2/messages/:messageId', Auth.verifyToken, Message.deleteUserMail);
+
+// Create or add a new group
+router.post('/api/v2/groups', Auth.verifyToken, Group.createGroup);
+
 
 
 module.exports = router;
---
+++ server/v2/models/messageTable.js
@@ -0,0 +1,76 @@
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+
+dotenv.config();
+
+let pool;
+const env = process.env.NODE_ENV;
+
+if (env === 'development') {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL_DEV,
+  });
+}
+if (env === 'test') {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL_TEST,
+  });
+} 
+if (env === 'production') {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL_PROD,
+  });
+}
+
+
+//  Create Messate Table
+
+const createMessageTable = async () => {
+  const messageTable = `CREATE TABLE IF NOT EXISTS
+      messageTable(
+        id SERIAL PRIMARY KEY,
+        createOn TIMESTAMP,
+        subject VARCHAR(128) NOT NULL,
+        message TEXT NOT NULL,
+        parentMessageId integer NOT NULL,
+        status VARCHAR(128) NOT NULL,
+        senderId VARCHAR(128) REFERENCES userTable(email),
+        receiverId VARCHAR(128) REFERENCES userTable(email),
+        senderDelete BOOLEAN NOT NULL,
+        receiverDelete BOOLEAN NOT NULL,
+        groupmail VARCHAR(128),
+        constraint check_status check (status in ('unread', 'read', 'draft'))
+      )`;
+
+  await pool.query(messageTable)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+};
+
+/**
+ * Drop User Table
+ */
+const dropMessageTable = async () => {
+  const dropMessageT = 'DROP TABLE IF EXISTS messageTable';
+  await pool.query(dropMessageT)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+};
+
+
+module.exports = {
+  createMessageTable,
+  dropMessageTable,
+};
