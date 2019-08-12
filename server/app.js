729bbefd94f75aa394f698fccc3a97bfd79a158c
vincentayorinde <vincentayorinde@yahoo.com>
2019-08-12 03:18:54 +0100

    setup cors

--- server/app.js
+++ server/app.js
@@ -2,6 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import swaggerUi from 'swagger-ui-express';
 import dotenv from 'dotenv';
+import session from 'express-session';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import 'idempotent-babel-polyfill';
 import swaggerDoc from '../docs/swagger.json';
 import router from './routes/index';
@@ -13,14 +16,32 @@ dotenv.config();
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser('secret'));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'secret',
+  }),
+);
 // Middleware
 app.use(router);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Epic Mail');
-});
+app.get('/', (req, res) => res.status(200).json({
+  status: 200,
+  message: 'welcome to Epic Mail',
+}));
+app.use((req, res) => res.status(404).json({
+  status: 404,
+  error: `Route ${req.url} Not found`,
+}));
+
+app.use((error, req, res) => res.status(500).json({
+  status: 500,
+  error,
+}));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 const PORT = process.env.PORT || 5000;
